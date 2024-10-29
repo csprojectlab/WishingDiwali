c8dbca97de59d4c956b2cc6000064f67ad31a4f3
csprojectlab <44633396+csprojectlab@users.noreply.github.com>
2024-10-29 16:16:12 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -7,16 +7,22 @@ class Particle {
       this.isRocket = isRocket;
       this.lifespan = 255;
     }
+
+    applyForce(force) {
+      this.acc.add(force);
+    }
     
     update() {
-      if ((this.pos.y<height-100 || this.vel.y>0) && this.isRocket) {
-        this.vel.add(this.acc);
-      }
-      else if (!this.isRocket) {
+      // if ((this.pos.y<height-100 || this.vel.y>0) && this.isRocket) {
+      //   this.vel.add(this.acc);
+      // }
+      if (!this.isRocket) {
         this.vel.mult(0.9);
-        this.lifespan -= 6;
+        this.lifespan -= 4;
       }
+      this.vel.add(this.acc);
       this.pos.add(this.vel);
+      this.acc.mult(0);
     }
     
     show() {
@@ -26,7 +32,9 @@ class Particle {
       }
       else {
         //fill(this.color);
-        ellipse(this.pos.x, this.pos.y, 3.5);
+        strokeWeight(2);
+        stroke(this.color, 255, 255, this.lifespan);
+        point(this.pos.x, this.pos.y);
         //fill(255);
       }
       //fill(255);
